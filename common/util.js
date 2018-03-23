f03870b306dacc5376fed01bd93ee4e97de78b0a
c4rlosc7 <camartinez7@hotmail.com>
2018-03-22 22:59:44 -0500

    add folder common with array message list

---
+++ common/util.js
@@ -0,0 +1,10 @@
+const USE_MOCKS_DATASOURCE = true;
+
+var tiposEntrada = [{
+    "id": "1",
+    "descripcion": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, vel fuga. Dolorum natus consequuntur repellendus accusamus, illum sit, saepe eos reprehenderit id rem blanditiis doloremque in iste harum, placeat, omnis."
+}, {
+    "id": "2",
+    "descripcion": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate magni perspiciatis nobis voluptates rem quisquam non, itaque, earum animi beatae doloribus consectetur fugit, debitis aut repudiandae nam dignissimos quod. Sit."
+}];
+
