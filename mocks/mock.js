43320986df0716e97eabcf3f2d4f875910fc7fc6
c4rlosc7 <camartinez7@hotmail.com>
2018-03-22 23:01:17 -0500

    add method mocks

---
+++ mocks/mock.js
@@ -0,0 +1,40 @@
+(function () {
+    'use strict'; // use as global $scope 
+
+    angular.module('edu.mocks.util', [])
+        .factory('utilMockFactory', function ($q) {
+
+    	    var methods = {}; 
+
+		    /** Transform a object to angular promise */
+    	    methods.objectToPromise = function (object) {
+    	    	var deferred = $q.defer();
+    	    	var result = {};
+                result.data   = {
+    	    		'successful': true,
+    	    		'exception': null,
+    	    		'payload': object,
+                    'message':''
+    	    	};
+    	    	deferred.resolve(result);
+    		    return deferred.promise;
+    	    };
+
+            /** Transform a object to angular promise with error*/
+            methods.objectToPromiseError = function (errorMessage) {
+                var deferred = $q.defer();
+                var result = {};
+                result.data   = {
+                    'successful': false,
+                    'exception': null,
+                    'payload': null,
+                    'message': errorMessage
+                };
+                deferred.resolve(result);
+                return deferred.promise;
+            };
+            return methods;
+            
+    });
+
+})();
